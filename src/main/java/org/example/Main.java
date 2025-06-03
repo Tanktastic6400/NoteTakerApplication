e5eeed84d1e41a709de543df1fe6d2b7f5739c00
Tanktastic6400 <159968548+Tanktastic6400@users.noreply.github.com>
2025-06-03 15:19:06 -0500

    setting up README for a basic beginning outline and the mission of the project.

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("The goal of this application is to create a computer based application that will start off with a java windows interface and then be integrated into a web browser");
     }
 }
